3d5a10954e61e90b155c5ae008f31a8feb42ca5e
eviljoel <eviljoel@linux.com>
2015-11-26 22:27:18 -0500

    Added TODOs.

--- src/opt/netcheck/netcheck.py
+++ src/opt/netcheck/netcheck.py
@@ -5,8 +5,11 @@ import subprocess
 import time
 import timber
 
+# TODO: Class level comment?
 class NetCheck:
+
     def __init__(self, config):
+
         self.config = config
         # TODO: Add sleep interval range config option, set it manually for now
         self.sleep_time = random.randrange(150)
@@ -15,36 +18,42 @@ class NetCheck:
         self.logger = timber.get_instance()
 
         # Instantiate NetworkManagerManager
-        self.network_meta = networkmetamanager.NetworkMetaManager(self.config['nmcli_timeout'], self.config['dig_timeout'])
+        self.network_meta = networkmetamanager.NetworkMetaManager(self.config['nmcli_timeout'])
 
+        # TODO: Remove comment?
         # Convenient /dev/null access
 
+    # Attempts a DNS query for query on nameserver via network.
+    #   Returns True if successful, None otherwise.
     def _DNS_query(self, network, nameserver, query):
         # TODO: dig timeout seems to be ignored?
-        # Attempts a DNS query for query on nameserver via network
-        # Returns True if successful, None otherwise
 
+        # TODO: So, I notice both of these check to make sure we have a valid connection to the
+        #   interface. If this isn't an expensive check, that is OK. However, if it isn't
+        #   you might want to pull the connection check out or combine these two calls.
         gateway_ip = self.network_meta.get_gateway_ip(network)
         interface_ip = self.network_meta.get_interface_ip(network)
         self.logger.trace('Querying %s for %s on network %s.' % (nameserver, query, network))
-        success = None
+        success = False
         # Add a route for this query, otherwise it will not use the specified network
+        # TODO: I would consider dropping '_list' from the variable name.
         route_command_list = ['ip', 'route', 'replace', '%s/32' % nameserver, 'via', gateway_ip]
         if(self.network_meta._subprocess_call(route_command_list)):
             # TODO: Figure out why you added this
             self.logger.debug('Route added.')
-            time.sleep(1)
+            time.sleep(1) # TODO: Make configurable? This is a really long time to sleep.
             dig_command_list = ['dig', '@%s' % nameserver, '-b', interface_ip, query, '+time=%d' % self.config['dig_timeout']]
             if(self.network_meta._subprocess_call(dig_command_list)):
                 self.logger.debug('DNS query successful.')
                 success = True
             else:
                 self.logger.debug('DNS query failed.')
-            
+ 
         else:
             self.logger.warn('Failed to set route for DNS lookup.')
+ 
         # Try to remove the route whether it failed or not
-        
+        # TODO: Change to check whether the call was successful or not for logging.
         self.network_meta._subprocess_call(['ip', 'route', 'del', nameserver])
         self.logger.debug('Removed route.')
         return success
@@ -52,17 +61,18 @@ class NetCheck:
 
     def _DNS_works(self, network):
         # Pick two exclusive-random choices from the nameserver and query list
-        # and call _DNS_query with them
+        #   and call _DNS_query with them.
         nameservers = random.sample(self.config['nameservers'], 2)
         queries = random.sample(self.config['dns_queries'], 2)
 
-        dns_works = None
+        dns_works = False
 
         self.logger.debug('First DNS query on %s.' % network)
         if(self._DNS_query(network, nameservers[0], queries[0])):
             dns_works = True
         else:
             self.logger.debug('Second DNS query on %s.' % network)
+            # TODO: Add an 'else' for logging.
             if(self._DNS_query(network, nameservers[1], queries[1])):
                 dns_works = True
 
--- src/opt/netcheck/networkmetamanager.py
+++ src/opt/netcheck/networkmetamanager.py
@@ -1,76 +1,85 @@
 import os
 import subprocess
 
+# TODO: Add a class level description.
 class NetworkMetaManager:
-    def __init__(self, nmcli_timeout, dig_timeout):
-        # Get NetworkManager major version and dump it to self.major_version
-        # nmcli parameters changed between 0.x and 1.x
-        raw_version = subprocess.check_output([ 'nmcli', '--version' ])
-        version_string = raw_version.split()[3]
-        self.major_version = int(version_string.split('.')[0])
+
+    def __init__(self, nmcli_timeout):
 
         # Set timeouts
         self.nmcli_timeout = nmcli_timeout
-        self.dig_timeout = dig_timeout
-        
+
+        # Get NetworkManager major version and dump it to self.major_version.
+        #   nmcli parameters changed between 0.x and 1.x.
+        raw_version = subprocess.check_output([ 'nmcli', '--version' ])
+        version_string = raw_version.split()[3]
+        self.major_version = int(version_string.split('.')[0])
+ 
+    # TODO: Method documentation
+    # Return True upon success, False otherwise.
+    #   Param command_list is a command split into a list, not a list of separate commands.
     def _subprocess_call(self, command_list):
-        # Run command with subprocess.call, redirecting output to /dev/null.
-        # Return True upon success, False otherwise.
-        # command_list is a command split into a list, not a list of separate commands.
+
         exit_code = None
         try:
+            # TODO: Move devnull back into self. Opening file handles probably isn't cheap.
             devnull = open(os.devnull, 'w')
+            # Run command with subprocess.call, redirecting output to /dev/null.
             exit_code = subprocess.call(command_list, stdin=devnull, stdout=devnull, stderr=devnull)
             devnull.close()
         except:
+            # TODO: Probably?
             # Eat exceptions because it will properly return False this way.
             pass
 
-        if(exit_code == 0):
-            return True
-        else:
-            return False
+        return exit_code == 0
 
+    # Try to connect to a network, return true on success, false on failure
     def connect(self, network_name):
-        # Try to connect to a network, return true on success, false on failure
-        # Different versions have different parameters
-        if(self.major_version == 0):
-            nmcli_command = ['nmcli', 'connection', 'up', 'id', network_name, '--timeout', str(self.nmcli_timeout)]
 
-        elif(self.major_version >= 1):
-            nmcli_command = ['nmcli', '-w', str(self.nmcli_timeout), 'connection', 'up', network_name]
+        # Different versions have different parameters
+        if (self.major_version < 1):
+            nmcli_command = ['nmcli', 'connection', 'up', 'id', network_name, '--timeout', '%d' % self.nmcli_timeout]
+
+        elif (self.major_version >= 1):
+            nmcli_command = ['nmcli', '-w', '%d' % self.nmcli_timeout, 'connection', 'up', network_name]
 
         return self._subprocess_call(nmcli_command)
 
+    # Try to disconnect a network.
     def disconnect(self, network_name):
-        # Try to disconnect a network.
-        if(self.major_version == 0):
+
+        if (self.major_version < 1):
             # disconnecting does not accept a timeout option, apparently.
             nmcli_command = ['nmcli', 'connection', 'down', 'id', network_name]
-        elif(self.major_version >= 1):
-            nmcli_command = ['nmcli', '-w', str(self.nmcli_timeout), 'connection', 'down', network_name]
+        elif (self.major_version >= 1):
+            nmcli_command = ['nmcli', '-w', '%d' % self.nmcli_timeout, 'connection', 'down', network_name]
 
         return self._subprocess_call(nmcli_command)
 
+    # Check for network connectivity, not including DNS
     def is_connected(self, network_name):
-        # Check for network connectivity, not including DNS
+
         nmcli_command = ['nmcli', 'c', 's']
-        if(network_name in subprocess.check_output(nmcli_command)):
+        if (network_name in subprocess.check_output(nmcli_command)):
             return True
         else:
             return False
 
+    # Get the current IP address for network_name's interface
     def get_interface_ip(self, network_name):
-        # Get the current IP address for network_name's interface
-        if(self.is_connected(network_name)):
+
+        if (self.is_connected(network_name)):
             # yucky text parsing
-            if(self.major_version == 0):
+            if (self.major_version < 1):
                 nmcli_command = ['nmcli', '-t', '-f', 'ip', 'connection', 'status', 'id', network_name]
+                # TODO: Explain in a larger comment what the returned format looks like.
+                #   Do this in similar places too.
                 ip_line = subprocess.check_output(nmcli_command).split('\n')[0]
                 ip_raw = ip_line.split()[2]
                 return ip_raw.split('/')[0]
 
-            elif(self.major_version >= 1):
+            elif (self.major_version >= 1):
                 # I can't test this on this system.  It should work though.
                 # TODO: Test this if we care enough to.
                 nmcli_command = ['nmcli', '-t', '-f', 'ip4.address', 'connection', 'show', network_name]
@@ -78,19 +87,23 @@ class NetworkMetaManager:
                 return ip_raw.split('/')[0]
         else:
             # TODO: Handle this case, throw in a log message.
+            # TODO: Instead of passing, explicitly return the value you want to return.
+            #   Actually, I would probably get rid of the multiple returns as I think multiple
+            #   returns tends to lead to hard to read code. Same for the next method.
             pass
-            
+ 
 
+    # Get the current gateway address for network_name
     def get_gateway_ip(self, network_name):
-        # Get the current gateway address for network_name
-        if(self.is_connected(network_name)):
+
+        if (self.is_connected(network_name)):
             # more yucky text parsing
-            if(self.major_version == 0):
+            if (self.major_version < 1):
                 nmcli_command = ['nmcli', '-t', '-f', 'ip', 'connection', 'status', 'id', network_name]
                 ip_line = subprocess.check_output(nmcli_command).split('\n')[0]
                 return ip_line.split()[5]
 
-            elif(self.major_version >= 1):
+            elif (self.major_version >= 1):
                 # I can't test this on this system either.
                 # TODO: Test this if we care enough to.
                 nmcli_command = ['nmcli', '-t', '-f', 'ip4.gateway', 'connection', 'show', network_name]
