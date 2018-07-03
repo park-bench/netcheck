cd18eef401f9e99e6be93fac2c5f42023230c0d2
Andrew Klapp <andklapp@null.net>
2018-07-02 21:14:40 -0500

    Fixed a line length.

--- src/usr/share/netcheck/netcheck-daemon.py
+++ src/usr/share/netcheck/netcheck-daemon.py
@@ -46,7 +46,8 @@ logger = logging.getLogger()
 # Now read the rest of them.
 config = {}
 config['dns_timeout'] = config_helper.verify_number_exists(config_file, 'dns_timeout')
-config['network_activation_timeout'] = config_helper.verify_number_exists(config_file, 'network_activation_timeout')
+config['network_activation_timeout'] = config_helper.verify_number_exists(
+    config_file, 'network_activation_timeout')
 config['sleep_range'] = config_helper.verify_number_exists(config_file, 'sleep_range')
 
 config['wired_network_name'] = config_helper.verify_string_exists(
