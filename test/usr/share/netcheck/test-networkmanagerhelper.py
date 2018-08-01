987ea285f71ad5fbc7e46410bac10beeb921011c
Andrew Klapp <andklapp@null.net>
2018-08-01 18:38:13 -0500

    Updated comments and license block.

--- test/usr/share/netcheck/test-networkmanagerhelper.py
+++ test/usr/share/netcheck/test-networkmanagerhelper.py
@@ -1,11 +1,27 @@
 #!/usr/bin/env python2
 
+# Copyright 2015-2018 Joel Allen Luellwitz and Andrew Klapp
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# This is not an automated test script. This is an ugly dumping ground for bits of code I
+#   have used to test the networkmanagerhelper module.
+
 import logging
 import NetworkManager
 import networkmanagerhelper
 
-### This is not an automated test script. This is an ugly dumping ground for bits of code I
-#   have used to test the networkmanagerhelper module.
 WIRED_TEST_NETWORK_NAME = 'ethernet-ens8'
 MISSING_TEST_NETWORK_NAME = 'please-dont-have-a-network-with-this-name'
 
