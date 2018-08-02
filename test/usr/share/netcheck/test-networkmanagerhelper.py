08a27a0e640fc00a75bb2b8d509bbca82af54129
Andrew Klapp <andklapp@null.net>
2018-08-01 19:58:24 -0500

    Updated test script for networkmanagerhelper.

--- test/usr/share/netcheck/test-networkmanagerhelper.py
+++ test/usr/share/netcheck/test-networkmanagerhelper.py
@@ -19,7 +19,6 @@
 #   have used to test the networkmanagerhelper module.
 
 import logging
-import NetworkManager
 import networkmanagerhelper
 
 WIRED_TEST_NETWORK_NAME = 'ethernet-ens8'
@@ -28,8 +27,11 @@ MISSING_TEST_NETWORK_NAME = 'please-dont-have-a-network-with-this-name'
 config = {}
 
 config['wired_interface_name'] = 'ens3'
-config['wireless_interface_name'] = 'ens8'
+config['wifi_interface_name'] = 'ens8'
 config['network_activation_timeout'] = 15
+
+config['wired_network_name'] = WIRED_TEST_NETWORK_NAME
+config['wifi_network_names'] = []
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger()
